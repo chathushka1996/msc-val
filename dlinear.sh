4379eb21d54ecc527725161024c8a713a9f4f3d5
Chathushka <chathushkahome9@gmail.com>
2024-09-10 09:29:14 +0530

    change

--- dlinear.sh
+++ dlinear.sh
@@ -22,7 +22,7 @@ do
     --is_training 1 \
     --root_path $root_path_name \
     --data_path $data_path_name \
-    --model_id $model_name_$seq_len'_'96 \
+    --model_id $model_name_$seq_len'_'$pred_len \
     --model $model_name \
     --data custom \
     --features M \
